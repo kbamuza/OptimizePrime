88910e064cce8b866ad5454df176b525efd2bfd8
Tanaka Chiura <u14034982@up.ac.za>
2018-05-30 18:42:26 +0200

    FixedMVC2

--- OptimizePrime/App_Start/RouteConfig.cs
+++ OptimizePrime/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OptimizePrime
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Status", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
